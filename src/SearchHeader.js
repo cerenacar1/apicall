b1aa6848f96a0069441e0e5d0874cf6f3a746125
Ceren Acar <ceremiacar@gmail.com>
2024-03-06 19:35:38 +0300

    App Çalışması

---
+++ src/SearchHeader.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function SearchHeader({ search }) {
+
+    const [value, setValue] = useState('')
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        debugger
+        search(value)
+
+    }
+
+    const handleChange =(event) =>{
+        setValue(event.target.value);
+
+    }
+    return (
+        <div className="searchDiv">
+            <form onSubmit={handleFormSubmit}>
+                <label>Ne Arıyorsunuz?</label>
+                <input value={value} onChange={handleChange}/>
+            </form>
+            
+        </div>
+    );
+}
+
+export default SearchHeader;
---
+++ src/components/ImageItem.js
@@ -0,0 +1,14 @@
+function ImageItem({image}) {
+  console.log(image)  
+    return ( 
+        <div>
+            <img className="imageListImg" 
+            src={image.urls.small} 
+            alt={image.alt_descripition} 
+            />
+
+        </div>
+     );
+}
+
+export default ImageItem;
